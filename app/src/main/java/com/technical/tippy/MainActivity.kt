bb38157f522de4a3b0a2cc6f454271ad67193a15
damac53 <mitchellefetty@gmail.com>
2023-06-25 15:48:51 +0200

    tip app

--- app/src/main/java/com/technical/tippy/MainActivity.kt
+++ app/src/main/java/com/technical/tippy/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.technical.tippy
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
 
+private const val TAG ="MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var etBaseAmount : EditText
     private lateinit var seekBarTip : SeekBar
-    private lateinit var tvTipPercent : TextView
+    private lateinit var tvTipPercentLabel : TextView
     private lateinit var tvTipAmount : TextView
     private lateinit var tvTotalAmount : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +21,25 @@ class MainActivity : AppCompatActivity() {
 
         etBaseAmount = findViewById(R.id.etBaseAmount)
         seekBarTip = findViewById(R.id.seekBarTip)
-        tvTipPercent = findViewById(R.id.tvTipPercentLabel)
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+
+
+        seekBarTip.setOnSeekBarChangeListener(object :  SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?,progress:Int, fromUser: Boolean) {
+              Log.i(TAG,"onProgressChanged $progress")
+                tvTipPercentLabel.text = "$progress%"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
     }
 }
