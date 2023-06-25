49acfc980ea69d509ef0f613d5152f266b7c3c93
damac53 <mitchellefetty@gmail.com>
2023-06-25 15:33:19 +0200

    tip app

--- app/src/main/java/com/technical/tippy/MainActivity.kt
+++ app/src/main/java/com/technical/tippy/MainActivity.kt
@@ -2,10 +2,24 @@ package com.technical.tippy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount : EditText
+    private lateinit var seekBarTip : SeekBar
+    private lateinit var tvTipPercent : TextView
+    private lateinit var tvTipAmount : TextView
+    private lateinit var tvTotalAmount : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        seekBarTip = findViewById(R.id.seekBarTip)
+        tvTipPercent = findViewById(R.id.tvTipPercentLabel)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
     }
 }
