f92c98d58d4aaf5adecb8286b9a88719585aaba5
damac53 <mitchellefetty@gmail.com>
2023-06-26 14:36:36 +0200

    tip app

--- app/src/main/java/com/technical/tippy/MainActivity.kt
+++ app/src/main/java/com/technical/tippy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.technical.tippy
 
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -9,6 +10,7 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 private const val TAG ="MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
@@ -81,6 +83,14 @@ class MainActivity : AppCompatActivity() {
             else->"Amazing"
         }
         tvTipDescription.text = tipDescription
+
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat()/ seekBarTip.max,
+            ContextCompat.getColor(this,R.color.color_worst_tip),
+            ContextCompat.getColor(this,R.color.color_best_tip),
+
+        )as Int
+        tvTipDescription.setTextColor(color)
     }
 
     private fun computeTipAndTotal() {
